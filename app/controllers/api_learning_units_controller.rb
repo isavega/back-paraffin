6f39b63d18ac3662fa54d186140fcd2129e9bc0c
cecyramallo <cecyramallo@gmail.com>
2022-10-05 12:57:55 -0300

    feat: comment index_completed_learning_units for now

--- app/controllers/api_learning_units_controller.rb
+++ app/controllers/api_learning_units_controller.rb
@@ -12,12 +12,12 @@ class ApiLearningUnitsController < ApiApplicationController
     render json: learning_unit, only: %i[id name description]
   end
 
-  def index_completed_learning_units
-    completed_learning_units = Curriculum
-                               .find(params[:curriculum_id])
-                               .learning_units
-                               .find(params[:learning_unit_id])
-                               .completed_learning_units
-    render json: completed_learning_units
-  end
+  # def index_completed_learning_units
+  #   completed_learning_units = Curriculum
+  #                              .find(params[:curriculum_id])
+  #                              .learning_units
+  #                              .find(params[:learning_unit_id])
+  #                              .completed_learning_units
+  #   render json: completed_learning_units
+  # end
 end

1affaeacfd1109550d5805bb91d09d8a887037f3
cecyramallo <cecyramallo@gmail.com>
2022-10-05 16:58:04 -0300

    feat: define method to mark a learning unit as completed

--- app/controllers/api_learning_units_controller.rb
+++ app/controllers/api_learning_units_controller.rb
@@ -12,6 +12,16 @@ class ApiLearningUnitsController < ApiApplicationController
     render json: learning_unit, only: %i[id name description]
   end
 
+  def create_completed_learning_unit
+    learning_unit = LearningUnit.find(params[:learning_unit_id])
+    completed_learning_unit = CompletedLearningUnit.create!(
+      user_id: params[:user_id],
+      learning_unit_id: params[:learning_unit_id]
+    )
+
+    render json: completed_learning_unit, status: :created
+  end
+
   # def index_completed_learning_units
   #   completed_learning_units = Curriculum
   #                              .find(params[:curriculum_id])

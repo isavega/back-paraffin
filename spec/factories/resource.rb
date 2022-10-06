5eb1325ff0d9a48c53a87b0ea06e84d55a2f3468
cecyramallo <cecyramallo@gmail.com>
2022-10-06 12:46:37 -0300

    test: create spec test for API endpoint to show a specific resource

--- spec/factories/resource.rb
+++ spec/factories/resource.rb
@@ -1,6 +1,8 @@
 FactoryBot.define do
   factory :resource do
     name { 'Ruby 101' }
+    url { 'https://example.com' }
+    description { 'The best Ruby resource' }
     association :user, factory: :user
     association :learning_unit, factory: :learning_unit
   end
